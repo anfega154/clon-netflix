c692bf5b8d0e08a3f1d33ee30cd5bd5295d7e18f
Andres <andres.ganan@mantum.com.co>
2023-07-10 11:23:28 -0500

    cambios

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+
+@extends('layouts.app')
+
+@section('contenido')
+
+<div class="container-login">
+    <h1>Entrar</h1>
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <input type="text" placeholder="Ingrese su email" name="email">
+        <input type="password" placeholder="password" name="password">
+
+        <button type="submit">Entrar</button>
+    </form>
+
+    <div class="help">
+        <div>
+            <input type="radio" id="me">
+            <label for="me">Lembre-se de mim</label>
+        </div>
+            
+        <a href="">Precisa de ajuda?</a>
+    </div>
+
+    <a href="" class="facebook"><img src="./assets/img/facebook.png" class="icon"/> Conectar com Facebook</a>
+
+    <div class="connect">
+        <p>Novo por aqui? <span id="span01">Assine agora</span>.</p>
+        <p>Esta página é protegida pelo Google reCAPTCHA para garantir que você não é um robô. <span id="span02">Saiba mais.</span></p>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,46 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-@yield('contenido')
+<!DOCTYPE html>
+<html lang="pt-br">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-  </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./assets/img/netflix-icon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="./css/login.css">
+
+    <title>Netflix-clone</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <img src="./assets/img/logo.png" alt="Logo">
+        </header>
+        @yield('contenido')
+
+        <footer>
+            <div class="container-footer">
+                <p>Dúvidas? <a href="">Ligue 0800-761-4631</a></p>
+
+                <div class="listas">
+                    <ul>
+                        <li><a href="">Perguntas frequentes</a></li>
+                        <li><a href="">Informações corporativas</a></li>
+                        <li><a href="">Preferências de cookies</a></li>
+                        <li><a href="">Centro de ajuda</a></li>
+                        <li><a href="">Privacidade</a></li>
+                        <li><a href="">Termos de uso</a></li>
+                    </ul>
+                </div>
+
+                <select>
+                    <option>Português</option>
+                    <option>Inglês</option>
+                </select>
+            </div>
+        </footer>
+    </div>
+    <script src="./js/login.js"></script>
+</body>
+
 </html>
